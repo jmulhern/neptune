922a8522174550d6f6b3cfe92dcd49ec55616881
John Mulhern <jmm@hey.com>
2020-09-05 11:26:37 -0700

    initial commit

---
+++ lib/neptune.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'neptune/version'
+
+module Neptune
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/neptune_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Neptune do
+  it 'has a version number' do
+    expect(Neptune::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
