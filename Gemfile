f3ce90591bd738e13f4b36172c808e543b0df360
John Mulhern <jmm@hey.com>
2020-09-05 12:22:51 -0700

    added coverage

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in neptune.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
-gem "rspec", "~> 3.0"
-gem "rubocop", "~> 0.90"
+gem 'rake', '~> 12.0', require: false
+gem 'rspec', '~> 3.0', require: false
+gem 'rubocop', '~> 0.90', require: false
+gem 'simplecov', '~> 0.19', require: false
--- spec/neptune_spec.rb
+++ spec/neptune_spec.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe Neptune do
+
+  subject { Neptune }
+
   it 'has a version number' do
-    expect(Neptune::VERSION).not_to be nil
+    expect(subject::VERSION).not_to be '0.1.0'
+  end
+
+  describe '#init' do
+    let(:window) { double }
+
+    it 'should call the show method on the window' do
+      expect(Window).to receive(:new).and_return(window)
+      expect(window).to receive(:show)
+
+      subject.init
+    end
   end
 end
